303eb24b993f4136edc47f5149a47510b38dd652
Billy <bc35786@gmail.com>
2023-07-13 18:00:43 -0500

    Add integration tests, refactor shop styles

--- src/App.js
+++ src/App.js
@@ -121,7 +121,6 @@ export const App = () => {
         return item;
       }
     });
-
     setCartItems(newCartItems);
   };
 
--- src/__tests__/integration.test.js
+++ src/__tests__/integration.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import "@testing-library/jest-dom";
 import { MemoryRouter } from "react-router-dom";
@@ -67,7 +67,6 @@ describe("checkout tests", () => {
   });
 
   test("remove button deletes item from cart", async () => {
-    const cartItems = [{ id: 1, watchName: "leather watch" }];
     render(
       <MemoryRouter initialEntries={["/checkout"]}>
         <TestApp />
@@ -78,17 +77,81 @@ describe("checkout tests", () => {
     await userEvent.click(screen.getAllByRole("button", { name: "Remove" })[0]);
     expect(screen.queryByText(/leather watch/)).not.toBeInTheDocument();
   });
+
+  test("increment button increases quanity of item in cart by 1 on each click", async () => {
+    render(
+      <MemoryRouter initialEntries={["/checkout"]}>
+        <TestApp />
+      </MemoryRouter>
+    );
+
+    const user = userEvent.setup();
+    const input = screen.getAllByRole("spinbutton", {
+      name: "quantity-input",
+    })[0];
+    expect(screen.queryAllByDisplayValue(1)).toHaveLength(2);
+    await user.click(screen.getAllByRole("button", { name: "+" })[0]);
+    expect(screen.getAllByDisplayValue(2)).toHaveLength(1);
+  });
+
+  test("decrement button decreases quantity of item in cart by 1 on each click", async () => {
+    render(
+      <MemoryRouter initialEntries={["/checkout"]}>
+        <TestApp />
+      </MemoryRouter>
+    );
+
+    const user = userEvent.setup();
+    expect(screen.getByDisplayValue(3)).toBeInTheDocument();
+    await user.click(screen.getAllByRole("button", { name: "-" })[1]);
+    expect(screen.getAllByDisplayValue(2)).toHaveLength(1);
+  });
+
+  test("decrement button removes item from cart when quanity is 1", async () => {
+    render(
+      <MemoryRouter initialEntries={["/checkout"]}>
+        <TestApp />
+      </MemoryRouter>
+    );
+    const user = userEvent.setup();
+    expect(screen.getByText(/rose gold watch/)).toBeInTheDocument();
+    await user.click(screen.getAllByRole("button", { name: "-" })[2]);
+    expect(screen.queryByText(/rose gold watch/)).not.toBeInTheDocument();
+  });
+
+  test("user can change item quanity by typing in input field", async () => {
+    render(
+      <MemoryRouter initialEntries={["/checkout"]}>
+        <TestApp />
+      </MemoryRouter>
+    );
+    const user = userEvent.setup();
+    const leatherWatchInput = screen.getAllByRole("spinbutton", {
+      name: "quantity-input",
+    })[0];
+    expect(screen.getAllByDisplayValue(1)).toHaveLength(2);
+    await user.type(leatherWatchInput, "4");
+    // expect(screen.getByDisplayValue(4)).toBeInTheDocument();
+    expect(leatherWatchInput).toHaveValue(14);
+  });
 });
 
-function TestApp() {
+const TestApp = () => {
   const initialCartItems = [
     {
       id: 1,
       watchName: "leather watch",
+      quantity: 1,
     },
     {
       id: 2,
       watchName: "silver watch",
+      quantity: 3,
+    },
+    {
+      id: 3,
+      watchName: "rose gold watch",
+      quantity: 1,
     },
   ];
   const [cartItems, setCartItems] = useState(initialCartItems);
@@ -97,12 +160,70 @@ function TestApp() {
     setCartItems(cartItems.filter((cartItem) => cartItem.id !== watchItem.id));
   };
 
+  const incrementCartItemQty = (cartItem) => {
+    const newCartItems = cartItems.map((item) => {
+      if (item.id === cartItem.id) {
+        console.log(item);
+        return { ...item, quantity: item.quantity + 1 };
+      } else {
+        return item;
+      }
+    });
+
+    setCartItems(newCartItems);
+  };
+
+  const decrementCartItemQty = (cartItem) => {
+    if (cartItem.quantity === 1) {
+      const newCartItems = cartItems.filter((item) => {
+        return item.id !== cartItem.id;
+      });
+      setCartItems(newCartItems);
+      return;
+    } else {
+      const newCartItems = cartItems.map((item) => {
+        if (item.id === cartItem.id) {
+          return { ...item, quantity: item.quantity - 1 };
+        } else {
+          return item;
+        }
+      });
+
+      setCartItems(newCartItems);
+    }
+  };
+
+  const handleItemQuantityInput = (e, cartItem) => {
+    const inputValue = +e.target.value;
+    console.log(typeof inputValue);
+    // if (typeof inputValue !== "number") {
+    //   console.log("must be a number");
+    //   return;
+    // } else if (inputValue < 1) {
+    //   console.log("value must be greater than 0");
+    //   return;
+    // }
+
+    const newCartItems = cartItems.map((item) => {
+      if (item.id === cartItem.id) {
+        return { ...item, quantity: inputValue };
+      } else {
+        return item;
+      }
+    });
+
+    setCartItems(newCartItems);
+  };
+
   return (
     <>
       <RouteSwitch
         cartItems={cartItems}
         removeWatchFromCart={removeWatchFromCart}
+        incrementCartItemQty={incrementCartItemQty}
+        decrementCartItemQty={decrementCartItemQty}
+        handleItemQuantityInput={handleItemQuantityInput}
       ></RouteSwitch>
     </>
   );
-}
+};
